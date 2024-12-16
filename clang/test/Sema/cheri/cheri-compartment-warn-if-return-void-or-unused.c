55f97ff70a53c07c80a8ce7039659bbd67b56e40
v01dxyz <v01dxyz@v01d.xyz>
2024-12-16 06:48:46 +0100

    Remove useless comments

--- clang/test/Sema/cheri/cheri-compartment-warn-if-return-void-or-unused.c
+++ clang/test/Sema/cheri/cheri-compartment-warn-if-return-void-or-unused.c
@@ -1,10 +1,6 @@
 // RUN: %clang_cc1 %s -o - -triple riscv32-unknown-unknown -emit-llvm -mframe-pointer=none -mcmodel=small -target-cpu cheriot -target-feature +xcheri -target-feature -64bit -target-feature -relax -target-feature -xcheri-rvc -target-feature -save-restore -target-abi cheriot -Oz -cheri-compartment=example -verify -fsyntax-only
 // RUN: %clang_cc1 %s -o - -triple riscv32-unknown-unknown -emit-llvm -mframe-pointer=none -mcmodel=small -target-cpu cheriot -target-feature +xcheri -target-feature -64bit -target-feature -relax -target-feature -xcheri-rvc -target-feature -save-restore -target-abi cheriot -Oz -cheri-compartment=example -fdiagnostics-parseable-fixits -fsyntax-only 2>&1 | FileCheck %s
 
-//  -fdiagnostics-parseable-fixits -fixit
-//
-
-//  -fdiagnostics-fixit-info
 __attribute__((cheri_compartment("example"))) void void_return_type_f(int a) // expected-warning{{void return on a cross-compartment call makes it impossible for callers to detect failure}} expected-note{{replace void return type with int}}
 {
   if (a) {
